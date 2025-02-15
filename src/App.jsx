a06e4cdab8fed6f57c7c577138348479b405a815
Barry210-hub <umarzafar49678@gmail.com>
2025-02-15 10:21:21 -0800

    root

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div>
         <Routes>
-          <Route  path='/'  element={  <Home />} />
+          <Route  path='/RMT'  element={  <Home />} />
         </Routes>
     </div>
   )
